cd4f59ab9cfb69e372b21b89ffd8c5a2a0ef35bc
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-08-26 03:54:03 -0300

    feat: Estrutura da Sidebar e Lesson finalizada

--- src/components/Lesson/index.tsx
+++ src/components/Lesson/index.tsx
@@ -1,7 +1,38 @@
-import './Style.scss'
+import './lesson.scss';
+import { FiCircle } from 'react-icons/fi';
+import { MdTimer } from 'react-icons/md';
+import { BsPlay } from 'react-icons/bs';
+import { RiCheckboxCircleFill } from 'react-icons/ri';
+import { useState } from 'react';
 
 export function Lesson() {
+  const [watched, setWatched] = useState(false);
+  console.log(watched);
+
   return (
-    <div></div>
-  )
+    <div className="lesson">
+      <button onClick={() => setWatched(!watched)}>
+        {watched ? (
+          <RiCheckboxCircleFill size={16} color="#0096fb" />
+        ) : (
+          <FiCircle size={16} color="#29292e"/>
+        )}
+      </button>
+
+      <div>
+        <h2>Introdução a lógica de programação</h2>
+        <div className="lesson__informations">
+          <span>
+            <BsPlay color="#29292e" />
+            <p>01</p>
+          </span>
+
+          <span>
+            <MdTimer color="#29292e" />
+            <p>03:43</p>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,16 @@
 import './sidebar.scss';
+import { Lesson } from '../Lesson';
 
 export function Sidebar() {
-  return <div className="sidebar"></div>;
+  return <div className="sidebar">
+    <Lesson />
+    <Lesson />
+    <Lesson />
+    <Lesson />
+    <Lesson />
+    <Lesson />
+    <Lesson />
+    <Lesson />
+    <Lesson />
+    </div>;
 }
